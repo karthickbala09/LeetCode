b48eb5fbe2d7720d242332d8767de5ca67eb1328
KARTHICK BALA S <skarthickbala2005@gmail.com>
2026-06-28 21:32:39 +0530

    Time: 0 ms (100%), Space: 48.5 MB (30.73%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        return bs(nums,0,nums.length-1,target);
+        
+    }
+    static int bs(int[] nums,int low,int high,int target){
+        if(low>high){
+            return -1;
+        }
+        int mid = low +(high-low)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(target > nums[mid]){
+            return bs(nums,mid+1,high,target);
+        }
+        else{
+            return bs(nums,low,mid-1,target);
+        }
+    }
+}
